9ad8752461a6edded126fad5491fe5381354bce5
Lucas Lima <lucasrafael123araujolima@gmail.com>
2022-04-23 16:37:18 -0400

    feat: :fire: Events

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,13 +1,49 @@
 class CalcController {
   constructor() {
+    this._locale = "pt-BR"
     this._displayCalcEl = document.querySelector("#display")
     this._dateEl = document.querySelector("#data")
     this._timeEl = document.querySelector("#hora")
     this._currentDate
     this.initialize()
+    this.initButtonsEvents()
   }
 
-  initialize() {}
+  initialize() {
+    this.setDisplayDateTime()
+    setInterval(() => {
+      this.setDisplayDateTime()
+    }, 1000)
+  }
+
+  addEventListenerAll(element, events, fn) {
+    events.split(" ").forEach(event => {
+      element.addEventListener(event, fn, false)
+    })
+  }
+
+  initButtonsEvents() {
+    let buttons = document.querySelectorAll("#buttons > g, #parts > g")
+
+    buttons.forEach((btn, index) => {
+      this.addEventListenerAll(btn, "click drag ", e => {
+        console.log(btn.className.baseVal.replace("btn-", ""))
+      })
+
+      this.addEventListenerAll(btn, "mouseover mouseup mousedown", e => {
+        btn.style.cursor = "pointer"
+      })
+    })
+  }
+
+  setDisplayDateTime() {
+    this.displayDate = this.currentDate.toLocaleDateString(this._locale, {
+      day: "2-digit",
+      month: "long",
+      year: "numeric"
+    })
+    this.displayTime = this.currentDate.toLocaleTimeString(this._locale)
+  }
 
   get displayTime() {
     return this._timeEl.innerHTML
