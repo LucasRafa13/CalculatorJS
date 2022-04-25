8eca4bbea04f74f7c84c1fb43d9f800b4cd0d8d0
Lucas Lima <lucasrafael123araujolima@gmail.com>
2022-04-25 09:10:46 -0400

    feat: :sparkles: Operações

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -5,13 +5,16 @@ class CalcController {
     this._displayCalcEl = document.querySelector("#display")
     this._dateEl = document.querySelector("#data")
     this._timeEl = document.querySelector("#hora")
+
     this._currentDate
+
     this.initialize()
     this.initButtonsEvents()
   }
 
   initialize() {
     this.setDisplayDateTime()
+
     setInterval(() => {
       this.setDisplayDateTime()
     }, 1000)
@@ -31,8 +34,33 @@ class CalcController {
     this._operation.pop()
   }
 
+  getLastOperation() {
+    return this._operation[this._operation.length - 1]
+  }
+
+  setLastOperation(value) {
+    this._operation[this._operation.length - 1] = value
+  }
+
+  isOperator(value) {
+    return ["+", "-", "*", "%", "/"].indexOf(value) > -1
+  }
+
   addOperation(value) {
-    this._operation.push(value)
+    if (isNaN(this.getLastOperation())) {
+      if (this.isOperator(value)) {
+        this.setLastOperation(value)
+      } else if (isNaN(value)) {
+        //Outra coisa
+        console.log("value")
+      } else {
+        this._operation.push(value)
+      }
+    } else {
+      let newValue = this.getLastOperation().toString() + value.toString()
+      this.setLastOperation(parseInt(newValue))
+    }
+
     console.log(this._operation)
   }
 
@@ -49,18 +77,25 @@ class CalcController {
         this.clearEntry()
         break
       case "soma":
+        this.addOperation("+")
         break
       case "subtracao":
+        this.addOperation("-")
         break
       case "divisao":
+        this.addOperation("/")
         break
       case "multiplicacao":
+        this.addOperation("*")
         break
       case "porcento":
+        this.addOperation("%")
         break
       case "igual":
         break
-
+      case "ponto":
+        this.addOperation(".")
+        break
       case "0":
       case "1":
       case "2":
@@ -73,10 +108,8 @@ class CalcController {
       case "9":
         this.addOperation(parseInt(value))
         break
-
       default:
         this.setError()
-        break
     }
   }
 
@@ -84,8 +117,9 @@ class CalcController {
     let buttons = document.querySelectorAll("#buttons > g, #parts > g")
 
     buttons.forEach((btn, index) => {
-      this.addEventListenerAll(btn, "click drag ", e => {
+      this.addEventListenerAll(btn, "click drag", e => {
         let textBtn = btn.className.baseVal.replace("btn-", "")
+
         this.execBtn(textBtn)
       })
 
@@ -121,11 +155,11 @@ class CalcController {
   }
 
   get displayCalc() {
-    return this._displayCalcEl.innerHTML
+    return this.displayCalcEl.innerHTML
   }
 
   set displayCalc(value) {
-    this._displayCalcEl.innerHTML = value
+    this.displayCalcEl.innerHTML = value
   }
 
   get currentDate() {
